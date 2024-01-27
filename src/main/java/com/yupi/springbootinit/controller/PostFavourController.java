c5bca3288aca77f0bd7b39b797e49deaf11571f2
paranoid <2376015252@qq.com>
2024-01-28 00:13:00 +0800

    提交

--- src/main/java/com/yupi/springbootinit/controller/PostFavourController.java
+++ src/main/java/com/yupi/springbootinit/controller/PostFavourController.java
@@ -26,8 +26,7 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * 帖子收藏接口
  *
- * @author <a href="https://github.com/liyupi">程序员鱼皮</a>
- * @from <a href="https://yupi.icu">编程导航知识星球</a>
+ * 提交
  */
 @RestController
 @RequestMapping("/post_favour")
